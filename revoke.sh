47e7f92fb6b8b20708cd981576d40de8bd3516a0
Brent Hopkins <contact@brenthopkins.me>
2021-11-25 02:34:58 -0600

    Create revoke.sh

---
+++ revoke.sh
@@ -0,0 +1,15 @@
+function revokeClient() {
+        cd /etc/openvpn/easy-rsa/ || return
+        ./easyrsa --batch revoke "$1"
+        EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
+        rm -f /etc/openvpn/crl.pem
+        cp /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn/crl.pem
+        chmod 644 /etc/openvpn/crl.pem
+        find /home/ -maxdepth 2 -name "$1.ovpn" -delete
+        rm -f "/root/$1.ovpn"
+        sed -i "/^$1,.*/d" /etc/openvpn/ipp.txt
+        cp /etc/openvpn/easy-rsa/pki/index.txt{,.bk}
+        sed -i -e '/^[R]/d' /etc/openvpn/easy-rsa/pki/index.txt
+}
+
+revokeClient $1
