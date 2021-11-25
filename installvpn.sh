deb7dce8584b0b402e6db82a10adebf9ac9528dd
Brent Hopkins <contact@brenthopkins.me>
2021-11-25 02:43:00 -0600

    Create installvpn.sh

---
+++ installvpn.sh
@@ -0,0 +1,286 @@
+#!/bin/bash
+# shellcheck disable=SC1091,SC2164,SC2034,SC1072,SC1073,SC1009
+
+# Modified version of Angristan's OpenVPN installer
+# https://github.com/angristan/openvpn-install
+
+function isRoot() {
+	if [ "$EUID" -ne 0 ]; then
+		return 1
+	fi
+}
+
+function tunAvailable() {
+	if [ ! -e /dev/net/tun ]; then
+		return 1
+	fi
+}
+
+function checkOS() {
+	if [[ -e /etc/debian_version ]]; then
+		OS="debian"
+		source /etc/os-release
+
+		if [[ $ID == "ubuntu" ]]; then
+			OS="ubuntu"
+			MAJOR_UBUNTU_VERSION=$(echo "$VERSION_ID" | cut -d '.' -f1)
+			if [[ $MAJOR_UBUNTU_VERSION -lt 20 ]]; then
+				echo "Only Ubuntu 20.04 or greater is supported."
+				exit 1
+			fi
+		fi
+	else
+		echo "Installer only supported on Ubuntu 20.04 or greater"
+		exit 1
+	fi
+}
+
+function initialCheck() {
+	if ! isRoot; then
+		echo "Sorry, you need to run this as root"
+		exit 1
+	fi
+	if ! tunAvailable; then
+		echo "TUN is not available"
+		exit 1
+	fi
+	checkOS
+}
+
+
+function installOpenVPN() {
+		PROTOCOL="tcp"
+		PORT="443"
+		PUBLIC_IP=$(curl -4 https://ifconfig.co)
+		ENDPOINT=${ENDPOINT:-$PUBLIC_IP}
+
+	# Run setup questions first, and set other variales if auto-install
+
+	# Get the "public" interface from the default route
+	NIC=$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)' | head -1)
+
+	# $NIC can not be empty for script rm-openvpn-rules.sh
+	if [[ -z $NIC ]]; then
+		echo
+		echo "Can not detect public interface."
+		echo "This needs for setup MASQUERADE."
+		until [[ $CONTINUE =~ (y|n) ]]; do
+			read -rp "Continue? [y/n]: " -e CONTINUE
+		done
+		if [[ $CONTINUE == "n" ]]; then
+			exit 1
+		fi
+	fi
+
+	# If OpenVPN isn't installed yet, install it. This script is more-or-less
+	# idempotent on multiple runs, but will only install OpenVPN from upstream
+	# the first time.
+	if [[ ! -e /etc/openvpn/server.conf ]]; then
+		if [[ $OS =~ (debian|ubuntu) ]]; then
+			apt-get update
+			apt-get -y install ca-certificates gnupg
+			apt-get install -y openvpn iptables openssl wget ca-certificates curl
+
+		fi
+		# An old version of easy-rsa was available by default in some openvpn packages
+		if [[ -d /etc/openvpn/easy-rsa/ ]]; then
+			rm -rf /etc/openvpn/easy-rsa/
+		fi
+	fi
+
+	# Find out if the machine uses nogroup or nobody for the permissionless group
+	if grep -qs "^nogroup:" /etc/group; then
+		NOGROUP=nogroup
+	else
+		NOGROUP=nobody
+	fi
+
+	# Install the latest version of easy-rsa from source, if not already installed.
+	if [[ ! -d /etc/openvpn/easy-rsa/ ]]; then
+		local version="3.0.7"
+		wget -O ~/easy-rsa.tgz https://github.com/OpenVPN/easy-rsa/releases/download/v${version}/EasyRSA-${version}.tgz
+		mkdir -p /etc/openvpn/easy-rsa
+		tar xzf ~/easy-rsa.tgz --strip-components=1 --directory /etc/openvpn/easy-rsa
+		rm -f ~/easy-rsa.tgz
+
+		cd /etc/openvpn/easy-rsa/ || return
+
+		echo "set_var EASYRSA_ALGO ec" >vars
+		echo "set_var EASYRSA_CURVE prime256v1" >>vars
+
+		# Generate a random, alphanumeric identifier of 16 characters for CN and one for server name
+		SERVER_CN="cn_$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)"
+		echo "$SERVER_CN" >SERVER_CN_GENERATED
+		SERVER_NAME="server_$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)"
+		echo "$SERVER_NAME" >SERVER_NAME_GENERATED
+
+		echo "set_var EASYRSA_REQ_CN $SERVER_CN" >>vars
+
+		# Create the PKI, set up the CA, the DH params and the server certificate
+		./easyrsa init-pki
+		./easyrsa --batch build-ca nopass
+
+		./easyrsa build-server-full "$SERVER_NAME" nopass
+		EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
+		
+		# tls-crypt
+		openvpn --genkey --secret /etc/openvpn/tls-crypt.key
+		
+	else
+		# If easy-rsa is already installed, grab the generated SERVER_NAME
+		# for client configs
+		cd /etc/openvpn/easy-rsa/ || return
+		SERVER_NAME=$(cat SERVER_NAME_GENERATED)
+	fi
+
+	# Move all the generated files
+	cp pki/ca.crt pki/private/ca.key "pki/issued/$SERVER_NAME.crt" "pki/private/$SERVER_NAME.key" /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn
+
+	# Make cert revocation list readable for non-root
+	chmod 644 /etc/openvpn/crl.pem
+
+	# Generate server.conf
+
+	echo "port $PORT
+proto tcp
+dev tun
+user nobody
+group $NOGROUP
+persist-key
+persist-tun
+keepalive 10 120
+topology subnet
+server 10.8.0.0 255.255.255.0
+ifconfig-pool-persist ipp.txt" >>/etc/openvpn/server.conf
+echo 'push "dhcp-option DNS 1.0.0.1"' >>/etc/openvpn/server.conf
+echo 'push "dhcp-option DNS 1.1.1.1"' >>/etc/openvpn/server.conf
+echo 'push "redirect-gateway def1 bypass-dhcp"' >>/etc/openvpn/server.conf
+echo "dh none
+ecdh-curve prime256v1
+tls-crypt tls-crypt.key
+crl-verify crl.pem
+ca ca.crt" >>/etc/openvpn/server.conf
+echo "cert $SERVER_NAME.crt" >>/etc/openvpn/server.conf
+echo "key $SERVER_NAME.key" >>/etc/openvpn/server.conf
+echo "auth SHA256
+cipher AES-128-GCM
+ncp-ciphers AES-128-GCM
+tls-server
+tls-version-min 1.2
+tls-cipher TLS-ECDHE-ECDSA-WITH-AES-128-GCM-SHA256
+client-config-dir /etc/openvpn/ccd
+status /var/log/openvpn/status.log
+verb 3" >>/etc/openvpn/server.conf
+
+	# Create client-config-dir dir
+	mkdir -p /etc/openvpn/ccd
+	# Create log dir
+	mkdir -p /var/log/openvpn
+
+	# Enable routing
+	echo 'net.ipv4.ip_forward=1' >/etc/sysctl.d/99-openvpn.conf
+	# Apply sysctl rules
+	sysctl --system
+
+
+	# Finally, restart and enable OpenVPN
+	# Don't modify package-provided service
+	cp /lib/systemd/system/openvpn\@.service /etc/systemd/system/openvpn\@.service
+	# Workaround to fix OpenVPN service on OpenVZ
+	sed -i 's|LimitNPROC|#LimitNPROC|' /etc/systemd/system/openvpn\@.service
+	# Another workaround to keep using /etc/openvpn/
+	sed -i 's|/etc/openvpn/server|/etc/openvpn|' /etc/systemd/system/openvpn\@.service
+
+	systemctl daemon-reload
+	systemctl enable openvpn@server
+	systemctl restart openvpn@server
+
+
+	# Add iptables rules in two scripts
+	mkdir -p /etc/iptables
+
+	# Script to add rules
+	echo "#!/bin/sh
+iptables -t nat -I POSTROUTING 1 -s 10.8.0.0/24 -o $NIC -j MASQUERADE
+iptables -I INPUT 1 -i tun0 -j ACCEPT
+iptables -I FORWARD 1 -i $NIC -o tun0 -j ACCEPT
+iptables -I FORWARD 1 -i tun0 -o $NIC -j ACCEPT
+iptables -I INPUT 1 -i $NIC -p $PROTOCOL --dport $PORT -j ACCEPT" >/etc/iptables/add-openvpn-rules.sh
+
+
+	# Script to remove rules
+	echo "#!/bin/sh
+iptables -t nat -D POSTROUTING -s 10.8.0.0/24 -o $NIC -j MASQUERADE
+iptables -D INPUT -i tun0 -j ACCEPT
+iptables -D FORWARD -i $NIC -o tun0 -j ACCEPT
+iptables -D FORWARD -i tun0 -o $NIC -j ACCEPT
+iptables -D INPUT -i $NIC -p $PROTOCOL --dport $PORT -j ACCEPT" >/etc/iptables/rm-openvpn-rules.sh
+
+
+	chmod +x /etc/iptables/add-openvpn-rules.sh
+	chmod +x /etc/iptables/rm-openvpn-rules.sh
+
+	# Handle the rules via a systemd script
+	echo "[Unit]
+Description=iptables rules for OpenVPN
+Before=network-online.target
+Wants=network-online.target
+
+[Service]
+Type=oneshot
+ExecStart=/etc/iptables/add-openvpn-rules.sh
+ExecStop=/etc/iptables/rm-openvpn-rules.sh
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target" >/etc/systemd/system/iptables-openvpn.service
+
+	# Enable service and apply rules
+	systemctl daemon-reload
+	systemctl enable iptables-openvpn
+	systemctl start iptables-openvpn
+
+	# If the server is behind a NAT, use the correct IP address for the clients to connect to
+	if [[ $ENDPOINT != "" ]]; then
+		IP=$ENDPOINT
+	fi
+
+	# client-template.txt is created so we have a template to add further users later
+	echo "client" >/etc/openvpn/client-template.txt
+	echo "proto tcp" >>/etc/openvpn/client-template.txt
+	echo "remote $IP $PORT
+dev tun
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+remote-cert-tls server
+verify-x509-name $SERVER_NAME name
+auth SHA256
+auth-nocache
+cipher AES-128-GCM
+tls-client
+tls-version-min 1.2
+tls-cipher TLS-ECDHE-ECDSA-WITH-AES-128-GCM-SHA256
+ignore-unknown-option block-outside-dns
+setenv opt block-outside-dns 
+verb 3" >>/etc/openvpn/client-template.txt
+
+	apt-get install -y vnstat vnstati
+	mkdir /var/openvpn_clients
+	mkdir /var/openvpn_scripts
+	wget -O /var/openvpn_scripts/revoke.sh https://raw.githubusercontent.com/bhopkins0/OpenVPN-Installer-For-Webadmin/main/revoke.sh
+	chmod +x /var/openvpn_scripts/revoke.sh
+	wget -O /var/openvpn_scripts/create.sh https://raw.githubusercontent.com/bhopkins0/OpenVPN-Installer-For-Webadmin/main/create.sh /var/openvpn_scripts/create.sh
+	chmod +x /var/openvpn_scripts/create.sh
+}
+
+# Check for root, TUN, OS...
+initialCheck
+
+# Check if OpenVPN is already installed
+if [[ -e /etc/openvpn/server.conf ]]; then
+	echo "OpenVPN already installed"
+else
+	installOpenVPN
+fi
